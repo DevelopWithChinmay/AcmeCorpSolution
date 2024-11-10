e127ca73a79ff01e3abbb30dd4b54fc6e31a8f7b
DevelopWithChinmay <develop.with.chinmay@gmail.com>
2024-11-10 17:37:34 +0530

    Customer orders should return 404 for invalid customer id

--- AcmeCorpApi/Controllers/CustomerOrdersController.cs
+++ AcmeCorpApi/Controllers/CustomerOrdersController.cs
@@ -1,4 +1,6 @@
 ﻿using AcmeCorpBusiness.Domain.Orders;
+using AcmeCorpBusiness.Entities;
+using AcmeCorpBusiness.Exceptions;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AcmeCorpApi.Controllers
@@ -8,9 +10,16 @@ namespace AcmeCorpApi.Controllers
     public class CustomerOrdersController : ControllerBase
     {
         [HttpGet("/{customerId}/orders")]
-        public async Task<IEnumerable<OrderFacade>> GetOrdersAsync([FromServices] OrderReadHandler handler, int customerId)
+        public async Task<ActionResult<IEnumerable<OrderFacade>>> GetOrdersAsync([FromServices] OrderReadHandler handler, int customerId)
         {
-            return await handler.QueryByCustomerIdAsync(customerId);
+            try
+            {
+                return Ok(await handler.QueryByCustomerIdAsync(customerId));
+            }
+            catch (ItemNotFoundException)
+            {
+                return new NotFoundResult();
+            }
         }
     }
 }
